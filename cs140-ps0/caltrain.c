bfdd2c24ce67906a234b90870c529c430195c0a3
Saurav Sachidanand <sauravsachidanand@gmail.com>
2017-11-22 13:28:21 +0530

    Finish caltrain

--- cs140-ps0/caltrain.c
+++ cs140-ps0/caltrain.c
@@ -1,29 +1,56 @@
 #include "pintos_thread.h"
 
 struct station {
-	// FILL ME IN
+	int waiting;
+	int boarding;
+	int seats;
+	struct lock l;
+	struct condition seats_avail;
+	struct condition boarding_done;
 };
 
 void
-station_init(struct station *station)
+station_init(struct station *s)
 {
-	// FILL ME IN
+	s->waiting = 0;
+	s->boarding = 0;
+	s->seats = 0;
+	lock_init((struct lock*)&s->l);
+	cond_init((struct condition*)&s->seats_avail);
+	cond_init((struct condition*)&s->boarding_done);
 }
 
 void
-station_load_train(struct station *station, int count)
+station_load_train(struct station *s, int count)
 {
-	// FILL ME IN
+	lock_acquire((struct lock*)&s->l);
+	s->seats = count;
+	cond_broadcast((struct condition*)&s->seats_avail, (struct lock*)&s->l);
+	while(s->boarding || (s->waiting && s->seats))
+		cond_wait((struct condition*)&s->boarding_done, (struct lock*)&s->l);
+	s->seats = 0;
+	lock_release((struct lock*)&s->l);
 }
 
 void
-station_wait_for_train(struct station *station)
+station_wait_for_train(struct station *s)
 {
-	// FILL ME IN
+	lock_acquire((struct lock*)&s->l);
+	s->waiting++;
+	while(!s->seats)
+		cond_wait((struct condition*)&s->seats_avail, (struct lock*)&s->l);
+	s->waiting--;
+	s->seats--;
+	s->boarding++;
+	lock_release((struct lock*)&s->l);
 }
 
 void
-station_on_board(struct station *station)
+station_on_board(struct station *s)
 {
-	// FILL ME IN
+	lock_acquire((struct lock*)&s->l);
+	s->boarding--;
+	if(!s->boarding && (!s->waiting || !s->seats))
+		cond_signal((struct condition*)&s->boarding_done, (struct lock*)&s->l);
+	lock_release((struct lock*)&s->l);
 }
