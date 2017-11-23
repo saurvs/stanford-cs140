c012b11d47c7df4564e07024fae74a10c39245ae
Saurav Sachidanand <sauravsachidanand@gmail.com>
2017-11-23 19:38:49 +0530

    Finish reaction

--- cs140-ps0/reaction.c
+++ cs140-ps0/reaction.c
@@ -6,23 +6,61 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+	int h_idle;
+	int o_idle;
+	int h_needed;
+	int o_needed;
+	struct lock l;
+	struct condition h_reactable;
+	struct condition o_reactable;
 };
 
 void
-reaction_init(struct reaction *reaction)
+reaction_init(struct reaction *r)
 {
-	// FILL ME IN
+	r->h_idle = 0;
+	r->o_idle = 0;
+	r->h_needed = 0;
+	r->o_needed = 0;
+	lock_init((struct lock*)&r->l);
+	cond_init((struct condition*)&r->h_reactable);
+	cond_init((struct condition*)&r->o_reactable);
 }
 
 void
-reaction_h(struct reaction *reaction)
+reaction_h(struct reaction *r)
 {
-	// FILL ME IN
+	lock_acquire((struct lock*)&r->l);
+	r->h_idle++;
+	while((r->h_idle < 2 || r->o_idle < 1) && !r->h_needed)
+		cond_wait((struct condition*)&r->h_reactable, (struct lock*)&r->l);
+	r->h_idle--;
+	if(r->h_needed){
+		r->h_needed--;
+	} else {
+		make_water();
+		r->h_needed++;
+		r->o_needed++;
+		cond_broadcast((struct condition*)&r->h_reactable, (struct lock*)&r->l);
+		cond_broadcast((struct condition*)&r->o_reactable, (struct lock*)&r->l);
+	}
+	lock_release((struct lock*)&r->l);
 }
 
 void
-reaction_o(struct reaction *reaction)
+reaction_o(struct reaction *r)
 {
-	// FILL ME IN
+	lock_acquire((struct lock*)&r->l);
+	r->o_idle++;
+	while(r->h_idle < 2 && !r->o_needed)
+		cond_wait((struct condition*)&r->o_reactable, (struct lock*)&r->l);
+	r->o_idle--;
+	if(r->o_needed){
+		r->o_needed--;
+	} else {
+		make_water();
+		r->h_needed += 2;
+		cond_broadcast((struct condition*)&r->h_reactable, (struct lock*)&r->l);
+	}
+	lock_release((struct lock*)&r->l);
 }
